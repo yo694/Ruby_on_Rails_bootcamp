7bec023f44cba236ff790451d4c3de4e8f899334
yogithakilari <yogitha.kilari@knoh.ai>
2026-08-19 11:29:12 +0000

    Fix redirects and add logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,21 @@
 class ApplicationController < ActionController::Base
+
   include Pundit::Authorization
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  def after_sign_in_path_for(_resource)
+    books_path
+  end
+
+  def after_sign_out_path_for(_resource_or_scope)
+    new_user_session_path
+  end
 
   private
 
   def user_not_authorized
     render json: { error: "Forbidden" }, status: :forbidden
   end
+
 end
